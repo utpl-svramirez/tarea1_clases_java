660a95062ff25788f89b6b4d9cf32e28b0a07faa
EC2 Default User <ec2-user@ip-172-31-40-252.ec2.internal>
2022-06-13 17:57:19 +0000

    actualizacion prueba

---
+++ categoria.java
@@ -0,0 +1,24 @@
+public class categoria{
+    
+    private int Codigo_Categ;
+    private String categoria; 
+    
+    //methods
+    
+    public void setCodigo_Categ(int Codigo_Categ{
+        this.Codigo_Categ = Codigo_Categ;
+    }
+    
+    public void getCodigo_Categoria (){
+        return this.Codigo_Categoria;
+    }
+    
+    public void setCategoria (String Categoria){
+        this.Categoria = Categoria;
+    }
+    
+    public void getCatedoria (){
+        return this.Categoria;
+    }
+
+}
--- juego.java
+++ juego.java
@@ -1,16 +1,49 @@
-//comentario
-
-public class Juego(){
-    int _codigo=0;
+public class juego{
     
+    private int Codigo;
+    private int Puntaje;
+    private Boolean Estado;
     
-    public void setCodigo(int codigo){
-        _codigo=codigo;
+    //methods
+    
+    public void setCodigo(int Codigo{
+        this.Codigo = Codigo;
     }
     
-     public void getCodigo(){
-        return _codigo;
+    public void getCodigo (){
+        return this.Codigo;
+    }
+    
+        public void setPuntaje(int Puntaje{
+        this.Puntaje = Puntaje;
+    }
+    
+    public void getPuntaje (){
+        return this.Puntaje;
+    }
+    
+    public void inicio(){
+        System.out.printIn ('Iniciar');
+    }
+    public void fin(){
+        System.out.printIn ('Finalizar');
+    }
+    public boolean consultarEstado
+        System.out.printIn('Iniciando..'');
+        return this.Estado
+        
+    public juego(){
+        
+    }
+    
+    public AgregarJuego(int codigo, int puntaje, boolean estado){
+        Codigo=codigo;
+        Puntaje=puntaje;
+        Estado=estado;
+        
+        //DB_AgregarJuego(Codigo,Puntaje,Estado)//
     }
     
     
+    //comentario
 }
