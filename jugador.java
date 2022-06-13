95bb00f7e798c9c12888a752b887c20322fec1ec
EC2 Default User <ec2-user@ip-172-31-40-252.ec2.internal>
2022-06-13 05:36:48 +0000

    new file uploaded

---
+++ jugador.java
@@ -0,0 +1,34 @@
+public class juego{
+    
+    private int Codigo;
+    private int Puntaje;
+    private Boolean Estado;
+    
+    //methods
+    
+    public void setCodigo(int Codigo{
+        this.Codigo = Codigo;
+    }
+    
+    public void getCodigo (){
+        return this.Codigo;
+    }
+    
+        public void setPuntaje(int Puntaje{
+        this.Puntaje = Puntaje;
+    }
+    
+    public void getPuntaje (){
+        return this.Puntaje;
+    }
+    
+    public void inicio(){
+        System.out.printIn ('Iniciar');
+    }
+    public void fin(){
+        System.out.printIn ('Finalizar');
+    }
+    public boolean consultarEstado
+        System.out.printIn('Iniciando..'');
+        return this.Estado
+}
