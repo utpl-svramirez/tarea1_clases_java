09634c67ede0ad665190573f2b25de179eb1b14c
EC2 Default User <ec2-user@ip-172-31-40-252.ec2.internal>
2022-06-21 01:22:56 +0000

    act de muestra

--- prueba.java
+++ prueba.java
@@ -1,2 +1,3 @@
 esto es una prueba para validar la carga correcta de archivos #take9
 esto es una nueva prueba de conexion para validar que todo se esté ejecutando
+esto es una nueva prueba para validar conexion git..
